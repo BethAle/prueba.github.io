23c4708b356581794d0bca18ee5ffc61bfe6e759
BethAle <129076550+BethAle@users.noreply.github.com>
2023-04-24 09:27:09 -0500

    main

---
+++ JS/operador.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Grados a Celcius</title>
+</head>
+<body>
+    \\Conversion larga 
+    var Celcius1= prompt("Introduzca la temperatur en ° Celcius");
+    Celsius = parseFloat (Celcius1);
+
+    \\Conversion corta
+    var Celcius1 = parse float (prompt(introduzca la temperatura en grados));
+
+    \\Procedo a hacer la conversion de Celcius a Farenheit: 
+    var Farenheit = 9/* Celsius1 + 32;
+    document.write("De Celcius a Farenheit es: ", Farenheit);
+</body>
+</html> 
