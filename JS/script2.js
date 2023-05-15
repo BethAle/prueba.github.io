69055e3fdf14402e757df653ebcd89c9a2557b59
BethAle <129076550+BethAle@users.noreply.github.com>
2023-05-15 09:22:13 -0500

    main

--- JS/script2.js
+++ JS/script2.js
@@ -1,3 +1,3 @@
-var fecha = prompt ("¿En qué año nació?")
-var año = 2023
-document.write("Su edad según el año en que nació es:" año - fecha)
+var fecha = parse int prompt ("¿En qué año nació?")
+var year = 2023
+document.write("Su edad según el año en que nació es:" year - fecha)
